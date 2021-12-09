e544a80c1d41a2b8ce4832df00eec4ab2648b0db
olaulvas <ola.ulvaas@gmail.com>
2021-12-09 13:42:31 +0100

    Finished

--- src/se/ulaolv/SnakeGame.java
+++ src/se/ulaolv/SnakeGame.java
@@ -2,6 +2,8 @@ package se.ulaolv;
 
 public class SnakeGame {
     public static void main(String[] args) {
+        //Olas SnakeGame
+
         //shortcut for
         //GameFrame frame = new GameFrame();
         new GameFrame();
